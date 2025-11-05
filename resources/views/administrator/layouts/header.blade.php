1d5290e2e5839fcc94a155179d88d89ef2d28704
syahreza <yazasyahreza@gmail.com>
2025-11-05 14:15:17 +0700

    perubahan header

--- resources/views/administrator/layouts/header.blade.php
+++ resources/views/administrator/layouts/header.blade.php
@@ -6,10 +6,6 @@
     <div class="navbar-collapse collapse">
         <ul class="navbar-nav navbar-align">
             <li class="nav-item dropdown">
-                <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
-                    <i class="align-middle" data-feather="settings"></i>
-                </a>
-
                 <a class="nav-link d-none d-sm-inline-block">
                     <span class="text-dark">Welcome, <strong>{{ Auth::user()->name }}!</strong></span>
                 </a>
